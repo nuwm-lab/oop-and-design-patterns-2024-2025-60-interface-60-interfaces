b27e5cd895d251cc349ea9633458068aecdc65a2
Admchik <127297619+Admchik@users.noreply.github.com>
2024-12-07 00:48:15 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,119 +7,53 @@ namespace LabWork
     // Необхідно змінювати і дописувати код лише в цьому проекті
     // Відео-інструкції щодо роботи з github можна переглянути 
     // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Human
+    public abstract class Person
     {
-        public string FirstName;
-        public string LastName;
-        public string MiddleName;
-        public int BirthDay;
-        public int BirthMonth;
-        public int BirthYear;
+        // Загальні властивості
+        protected string FirstName;
+        protected string LastName;
+        protected string Patronymic;
+        protected int BirthDay;
+        protected int BirthMonth;
+        protected int BirthYear;
 
-        // Віртуальний метод для введення даних
-        public virtual void InputData()
+        // Абстрактні властивості
+        public abstract string FirstName { get; set; }
+        public abstract string LastName { get; set; }
+        public abstract string Patronymic { get; set; }
+
+        // Загальні методи
+        public virtual void SetPersonalData()
         {
-            Console.Write("Введіть ім'я: ");
+            Console.WriteLine("Enter your first name: ");
             FirstName = Console.ReadLine();
-
-            Console.Write("Введіть прізвище: ");
+            Console.WriteLine("Enter your last name: ");
             LastName = Console.ReadLine();
-
-            Console.Write("Введіть по-батькові: ");
-            MiddleName = Console.ReadLine();
-
-            Console.Write("Введіть день народження: ");
-            BirthDay = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Введіть місяць народження: ");
-            BirthMonth = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Введіть рік народження: ");
+            Console.WriteLine("Enter your patronymic: ");
+            Patronymic = Console.ReadLine();
+            Console.WriteLine("Enter your birth year: ");
             BirthYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your birth month: ");
+            BirthMonth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your birth day: ");
+            BirthDay = Convert.ToInt32(Console.ReadLine());
         }
 
-        // Віртуальний метод для обчислення віку
-        public virtual void CalculateAge()
+        public virtual int CalculateAge()
         {
-            DateTime currentDate = DateTime.Now;
-            int age = currentDate.Year - BirthYear;
+            int currentYear = DateTime.Now.Year;
+            int currentMonth = DateTime.Now.Month;
+            int currentDay = DateTime.Now.Day;
 
-            if (currentDate.Month < BirthMonth ||
-                (currentDate.Month == BirthMonth && currentDate.Day < BirthDay))
+            int age = currentYear - BirthYear;
+            if (currentMonth < BirthMonth || (currentMonth == BirthMonth && currentDay < BirthDay))
             {
                 age--;
             }
-
-            Console.WriteLine($"Вік людини: {age} років");
+            return age;
         }
 
-        // Віртуальний метод для підрахунку літер
-        public virtual void CountLetterOccurrences()
-        {
-            Console.Write("Введіть літеру для пошуку в прізвищі: ");
-            char letter = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-
-            int count = 0;
-            foreach (char c in LastName)
-            {
-                if (char.ToLower(c) == char.ToLower(letter))
-                {
-                    count++;
-                }
-            }
-
-            Console.WriteLine($"Кількість входжень літери '{letter}' у прізвищі: {count}");
-        }
+        public abstract int CountLetterInLastName(char letter);
     }
 
-    // Похідний клас
-    class ExtendedHuman : Human
-    {
-        public string FavoriteHobby;
-
-        // Перевизначення методу для введення даних
-        public override void InputData()
-        {
-            base.InputData();
-
-            Console.Write("Введіть улюблене хобі: ");
-            FavoriteHobby = Console.ReadLine();
-        }
-
-        // Перевизначення виводу додаткової інформації
-        public override void CountLetterOccurrences()
-        {
-            base.CountLetterOccurrences();
-            Console.WriteLine($"Улюблене хобі: {FavoriteHobby}");
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Виберіть режим: 1 - Базова людина, 2 - Розширена людина");
-            char userChoice = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-
-            // Динамічне створення об'єкту
-            Human person;
-            if (userChoice == '1')
-            {
-                person = new Human();
-            }
-            else
-            {
-                person = new ExtendedHuman();
-            }
-
-            // Використання віртуальних методів
-            person.InputData();
-            person.CalculateAge();
-            person.CountLetterOccurrences();
-
-            Console.WriteLine("\nПрограму завершено.");
-        }
-    }
 }
